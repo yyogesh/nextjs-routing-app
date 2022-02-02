e5bb2ec6dabe53d66be734d353dfcda5321ff5bf
yyogesh <yogeshggn1@gmail.com>
2022-02-02 09:32:22 +0530

    404, programming routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Link from 'next/link'
+import { useRouter } from 'next/router';
 
 const Home = () => {
-  return <div>Home NextJS</div>;
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/product');
+  }
+  return <div>
+    <h1>Home</h1>
+    <div>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </div>
+    <div>
+      <Link href="/product">
+        <a>Product</a>
+      </Link>
+    </div>
+    <div>
+      <button onClick={handleClick}>Place order</button>
+    </div>
+  </div>;
 };
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Link from 'next/link'
 
-const Product = () => {
+const Product = ({ productId = 100 }) => {
     return <div>
         <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
+            <li><Link href='/product/1'><a>Product 1</a></Link></li>
+            <li><Link href='/product/2'><a>Product 2</a></Link></li>
+            <li><Link href='/product/3'><a>Product 3</a></Link></li>
+            <li><Link href={`/product/${productId}`}><a>Product {productId}</a></Link></li>
         </ul>
     </div>;
 };
