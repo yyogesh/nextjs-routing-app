63905fe1ec13570e73f483c0c7f0bb3bb44d9e53
yyogesh <yogeshggn1@gmail.com>
2022-02-04 17:22:59 +0530

    nextjs getStaticPaths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -11,14 +11,31 @@ function Post({ post }) {
 
 export default Post;
 
+export async function getStaticPaths() {
+    return {
+        paths: [
+            {
+                params: { postId: '1' }
+            },
+            {
+                params: { postId: '2' }
+            },
+            {
+                params: { postId: '3' }
+            }
+        ],
+        fallback: false,
+    }
+}
+
 export async function getStaticProps(context) {
     const { params } = context;
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts${params.postId}`);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
     const data = await response.json();
 
     return {
         props: {
-            post: data.slice(0, 3)
+            post: data
         }
     }
 }
